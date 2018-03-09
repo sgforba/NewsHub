75d19c4907515233ae22e9dd92b2ac04de9c12e4
Unknown <sgforba@gmail.com>
2018-03-08 22:18:21 -0500

    main modal functionality

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -2,14 +2,33 @@ import React, { Component } from 'react';
 
 
 class Post extends Component {
-    
+
+    constructor(props) {
+        super (props)
+        this.state = {modal: false};
+        this.modalToggle = this.modalToggle.bind(this);
+    }
+
+    modalToggle() {
+        this.setState({
+            modal: !this.state.modal
+        });
+        if(!document.getElementById('main').classList.contains("modal")){
+            document.getElementById('main').classList.add('modal');
+        }
+        else{
+            document.getElementById('main').classList.remove('modal');
+        }
+    }
+
     render() {
         const thumbStyle = {
             display: 'block',
             background: `url(${this.props.post.image})`
-          };           
+        }  
+    
         return(
-            <div className={`post ${ this.props.post.source }`}>
+            <div className={`post ${ this.props.post.source } modal-${ this.state.modal }`}>
                 <div className="thumb-wrapper">
                     <a className="thumbnail" style={thumbStyle}></a>
                     <a href={this.props.post.url} className="cta-link" target="_blank">Read More</a>
@@ -18,8 +37,10 @@ class Post extends Component {
                 <span className={`icon icon-${ this.props.post.source }`}>{ this.props.post.source }</span>
                 <a href={this.props.post.url} target="_blank" className="title">{this.props.post.title}</a>
                  <p className="content-wrapper">
-                 {this.props.post.content}
-                 </p>      
+                    {this.props.post.content}
+                 </p>  
+                <div className="close-modal"onClick={this.modalToggle}>x</div>
+                <div className="open-modal" onClick={this.modalToggle}>Read More..</div>
             </div>
         );        
     }
